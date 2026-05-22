25cebc520d439ce5da840057a831545d70888c79
János Wacha <janos.wacha@gmail.com>
2026-05-22 18:20:11 +0200

    There are the bones of the project

---
+++ WRO/engine.py
@@ -0,0 +1,11 @@
+class Engine:
+    def __init__(self):
+        ... # Initialize engine attributes here
+    
+    def move_forward(self, distance):
+        """Move the robot forward by a specified distance."""
+        ... # Implementation for moving forward
+    
+    def turn(self, angle):
+        """Turn the steering wheel by a specified angle."""
+        ... # Implementation for turning the steering wheel
---
+++ WRO/sensor.py
@@ -0,0 +1,7 @@
+class Sensor:
+    def __init__(self):
+        ... # Initialize sensor attributes here
+    
+    def read_data(self):
+        """Get the nearest object data from the sensors."""
+        ... # Implementation for reading sensor data
