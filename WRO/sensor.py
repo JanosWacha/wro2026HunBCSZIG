a20980ef89b4b33a0548751d2385602c71cce4a0
GDHappybird2 <vlasits.bendek2@gmail.com>
2026-06-18 19:29:16 +0200

    Delete WRO/sensor.py

--- WRO/sensor.py
+++
@@ -1,7 +0,0 @@
-class Sensor:
-    def __init__(self):
-        ... # Initialize sensor attributes here
-    
-    def read_data(self):
-        """Get the nearest object data from the sensors."""
-        ... # Implementation for reading sensor data
